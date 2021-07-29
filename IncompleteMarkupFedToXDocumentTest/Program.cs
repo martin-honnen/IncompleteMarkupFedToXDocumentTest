1a8f792689a99cddccf2073a9e66eda63ea1b6a3
Martin Honnen <martin.honnen@gmx.de>
2021-07-29 08:36:14 +0200

    Projektdateien hinzufügen.

---
+++ IncompleteMarkupFedToXDocumentTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace IncompleteMarkupFedToXDocumentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xml = @"<root><section><p>Paragraph 1.</p><p>Paragraph 2";
+
+            XDocument doc = new XDocument();
+
+            try
+            {
+                using (StringReader sr = new StringReader(xml))
+                {
+                    using (XmlReader xr = XmlReader.Create(sr))
+                    {
+                        using (XmlWriter xw = doc.CreateWriter())
+                        {
+                            xw.WriteNode(xr, false);
+                        }
+                    }
+                }
+                Console.WriteLine("Assembled XML without parsing error: {0}", doc);
+            }
+            catch (XmlException e)
+            {
+                Console.WriteLine("XML parsing error: {0}", e.Message);
+                Console.WriteLine("Assembled XML before parsing error: {0}", doc);
+            }
+        }
+    }
+}
